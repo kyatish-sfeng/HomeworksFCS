379e1a6a1a68180b3aebcd3357f17c0bb7d4d54c
ykneni <y2kneni@gmail.com>
2015-09-10 17:19:28 -0400

    Dummy commit

--- HW2/Conjugates.cs
+++ HW2/Conjugates.cs
@@ -22,6 +22,7 @@ namespace HW2
              * from 1 to lenght of string
              * substring of i-1 = a and remaining =b
              * concatinate b and a
+             * Dummy edit
              * verify that ba contains second input           
              */
             for (int i = 1; i <= lenghtOfString; i++)
