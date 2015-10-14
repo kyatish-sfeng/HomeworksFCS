c6747407363ec0406c270775e58f9ea08b74283b
ykneni <y2kneni@gmail.com>
2015-10-13 21:46:12 -0400

    Re-commit hw3

---
+++ HW3/MinimumWeakPeriodDiagram.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    public class MinimumWeakPeriodDiagram
+    {
+        public char[][] PWordPresentation { get; set; }
+
+        public void ConstructPWordWithWkPeroid(string pword, int periodicity)
+        {
+            int lenghtOfWord = pword.Length;
+            char[] pwordChar=pword.ToCharArray();
+            int rows = 0;
+            if (lenghtOfWord % periodicity == 0)
+            {
+                rows = lenghtOfWord / periodicity;
+
+            }
+            else
+            {
+                rows = (lenghtOfWord / periodicity)+1;
+            }
+            PWordPresentation = new char[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (i == rows - 1)
+                {
+                    int wordRemainder = lenghtOfWord % periodicity;
+                    PWordPresentation[i] = new char[wordRemainder];
+                    for (int a = 0; a < wordRemainder; a++)
+                    {
+                        PWordPresentation[i][a] = pwordChar[(i * periodicity) + a];
+                    }
+                }
+                else
+                {
+                    PWordPresentation[i] = new char[periodicity];
+                    for (int j = 0; j < periodicity; j++)
+                    {
+                        PWordPresentation[i][j] = pwordChar[(i * periodicity) + j];
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ HW3/WordConstruct.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    public class WordConstruct
+    {
+        List<int> IIterations = null;
+        List<string> IWords = null;
+        public void ComputeIList(int p)
+        {
+            IIterations = new List<int>();
+            for (int i = 2; i <= p; i++)
+            {
+                IIterations.Add(i);
+            }
+        }
+
+        public void ConstructIWord(int p, int q)
+        {
+            foreach (var item in IIterations)
+            {
+                int lengthOfWord = p + q - item;
+                int totalDistinctLetter = item;
+
+            }
+        }
+
+        public void ConstructIWordWithPeriodicity()
+        {
+            //number of rows (total length -p) 
+            //char[][] 
+        }
+    }
+}
